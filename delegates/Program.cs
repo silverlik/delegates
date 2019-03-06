af6f494a1611eaec5fdcf2597ac53c77946ba863
Helena Sysoeva <alfeya@hotmail.com>
2019-03-05 20:48:10 -0500

    simple delegate

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -2,11 +2,26 @@
 
 namespace delegates
 {
+    
+   
+        
     class Program
-    {
+    { public static int Plus(int x, int y) => x + y;
+
+        public static int Multi(int x, int y) => x * y;
+    
+        delegate int Function(int x, int y);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Function f = Plus;
+            
+            Console.WriteLine("Plus" + f(10,20));
+            f = Multi;
+            Console.WriteLine("Multi" + f(10, 20));
+            f = delegate (int x, int y) { return x - y; };
+            Console.WriteLine("Sub" + f(10, 20));
+
+            Console.ReadLine();
         }
     }
 }
