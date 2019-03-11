6cc7ca6151a91d8b6544e3ad8f6a77ac5341a236
Helena Sysoeva <alfeya@hotmail.com>
2019-03-11 14:27:04 -0400

    delegate sample: select delegate by choosen zone

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -1,25 +1,48 @@
 ﻿using System;
 
 namespace delegates
-{
-    
-   
+{       
+    public static class Zones
+    {
+        public static float Zone1(float price) => price * (float)0.25;
+        public static float Zone2(float price) => price * (float)0.12 + 25;
+        public static float Zone3(float price) => price * (float)0.08;
+        public static float Zone4(float price) => price * (float)0.04 + 25;
         
-    class Program
-    { public static int Plus(int x, int y) => x + y;
-
-        public static int Multi(int x, int y) => x * y;
-    
-        delegate int Function(int x, int y);
+    }
+   class Program
+    {   
+        delegate float Shipping(float x);
         static void Main(string[] args)
         {
-            Function f = Plus;
-            
-            Console.WriteLine("Plus" + f(10,20));
-            f = Multi;
-            Console.WriteLine("Multi" + f(10, 20));
-            f = delegate (int x, int y) { return x - y; };
-            Console.WriteLine("Sub" + f(10, 20));
+            Shipping f = null;
+            string s;
+            do
+            {
+                Console.WriteLine("Please enter zone");
+                s = Console.ReadLine();
+                if (s.Equals("exit")) break;
+                Console.WriteLine("Please enter a price of the item");
+                var s1 = Console.ReadLine();
+                if (float.TryParse(s1, out float price))
+                {
+                  
+                    switch (s)
+                    {
+                        
+                        case "zone1": f = Zones.Zone1; break;
+                        case "zone2": f = Zones.Zone2; break;
+                        case "zone3": f = Zones.Zone3; break;
+                        case "zone4": f = Zones.Zone4; break;
+                    }
+                    if (f != null)
+                    {
+                        Console.WriteLine("Cost is {0:C}",f(price));
+                    }
+                    else Console.WriteLine("You've entered a wrong zone!");
+                }
+            } while (s != "exit");
+           
 
             Console.ReadLine();
         }
